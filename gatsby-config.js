c02dd06da8672f5e63acadc709c4e4112fbf8f3b
David Nguyen <d_nguyen@berkeley.edu>
2017-09-22 18:11:39 -0700

    first commit

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  siteMetadata: {
+    title: `InnoD Web Starter`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sass`,
+    `gatsby-plugin-eslint`,
+  ],
+}
