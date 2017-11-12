51ee6bc73a643b7e2d5d8fe841168d33db52ed03
Cindy Tung <cindytung@berkeley.edu>
2017-11-12 14:14:32 -0800

    social media footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import FontAwesome from 'react-fontawesome';
+import '../styles/footer.scss';
+
+export default class Footer extends Component {
+  /* eslint-disable jsx-a11y/no-static-element-interactions */
+  render() {
+    return (
+      <div className="footer">
+        <div className="media">
+          <a href="https://www.facebook.com/groups/139920413179840/" target="_blank" rel="noopener noreferrer">
+            <FontAwesome
+              className="fb__icon"
+              name="facebook"
+            />
+          </a>
+          <a href="mailto:ircofberkeley@gmail.com" target="_top" rel="noopener noreferrer">
+            <FontAwesome
+              className="mail__icon"
+              name="envelope-o"
+            />
+          </a>
+        </div>
+      </div>
+    );
+  }
+}
--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Footer from '../components/Footer';
 import Navbar from '../components/Navbar';
 
 import '../styles/base.scss';
 
 const TemplateWrapper = ({ children }) => (
   <div>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
     <Navbar />
+    <Footer />
     <div>
       {children}
     </div>
