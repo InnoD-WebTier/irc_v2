82bc0c5d554da6146e2747315b68ac049c63d57a
Emily Gosti <egosti@berkeley.edu>
2017-11-16 17:36:24 -0800

    added more to home page

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,16 +1,55 @@
 import React from "react";
 import '../styles/homepage.scss';
+import OrganizationPhotoGroup from '../components/OrganizationPhotoGroup';
+import IRCActivity from '../components/IRCActivity';
 import background from '../assets/images/background.png';
+import casecomp from '../assets/images/case_competition.png';
+import blog from '../assets/images/blog.png';
+import journal from '../assets/images/journal.png';
 
 const HomePage = () => {
   return (
-    <div className='TitleScreen'>
-      <img className='Background' src={background} alt='background' />
-      <div className='Title'>
-        The International Relations Council of Berkeley
+    <div>
+      <div className='TitleScreen'>
+        <img className='Background' src={background} alt='background' />
+        <div className='Title'>
+          The International Relations Council of Berkeley
+        </div>
+        <div className='Subtitle'>
+          Facilitating dialogue on global issues between on-campus student organizations.
+        </div>
       </div>
-      <div className='Subtitle'>
-        Facilitating dialogue on global issues between on-campus student organizations.
+      <div className='OrgsScreen'>
+        <div className='Title'>
+          Founded in 2016 by five UC Berkeley student organizations:
+        </div>
+        <OrganizationPhotoGroup />
+        <div className='OurHistory'>
+          <div className='ButtonTitle'>Our History</div>
+        </div>
+      </div>
+      <div className='InfoScreen'>
+        <div className='Title'>
+          What We Do
+        </div>
+        <IRCActivity
+          name="IRC Case Competition"
+          description="We invite teams of Berkeley students to develop innovative solutions to
+          the most pressing problems in international relations and developmental politics."
+          img={casecomp}
+        />
+        <IRCActivity
+          name="IRC Blog"
+          description="The Global Citizen is a student-run blog about all things international
+          relations, updated bi-monthly. We're looking for new contributors!"
+          img={blog}
+        />
+        <IRCActivity
+          name="IRC Journal"
+          description="The International Relations Review of Berkeley is published semesterly.
+          This semester's theme is &quot;International Law and Disorder&quot;."
+          img={journal}
+        />
       </div>
     </div>
   );
--- src/components/OrganizationPhotoGroup.jsx
+++ src/components/OrganizationPhotoGroup.jsx
@@ -1,29 +1,41 @@
 import React from "react";
 import '../styles/organizationphoto.scss';
 import OrganizationPhoto from '../components/OrganizationPhoto';
+import bmun from '../assets/images/BMUN.png';
+import ucbmun from '../assets/images/UCBMUN.png';
+import sip from '../assets/images/SIP.png';
+import dps from '../assets/images/DPS.png';
+import amnesty from '../assets/images/Amnesty.png';
 
 const OrganizationPhotoGroup = () => {
   return (
-    <div>
+    <div className='PhotoWrapper'>
       <OrganizationPhoto
         name="BMUN"
-        img="http://via.placeholder.com/150x150"
+        description="Berkeley Model United Nations is a student-run organization
+        on the UC Berkeley campus the runs the world's premier -- and very first --
+        United Nations simulation."
+        img={bmun}
       />
       <OrganizationPhoto
         name="UCBMUN"
-        img="http://via.placeholder.com/150x150"
+        description="description here"
+        img={ucbmun}
       />
       <OrganizationPhoto
         name="SIP"
-        img="http://via.placeholder.com/150x150"
+        description="description here"
+        img={sip}
       />
       <OrganizationPhoto
         name="Delta Phi Epsilon"
-        img="http://via.placeholder.com/150x150"
+        description="description here"
+        img={dps}
       />
       <OrganizationPhoto
         name="Amnesty International"
-        img="http://via.placeholder.com/150x150"
+        description="description here"
+        img={amnesty}
       />
     </div>
   );
