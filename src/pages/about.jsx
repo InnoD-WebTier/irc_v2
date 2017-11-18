37ab708a21dcee75e751b142aa0ba3fbf1bdaea0
Christopher Franco <monterrosa@eecs.berkeley.edu>
2017-11-17 16:42:03 -0800

    added history text

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -8,6 +8,16 @@ export default class About extends React.Component {
       <div>
         <div>
           <h1>Our History</h1>
+          <p>The International Relations Council (IRC) of Berkeley was founded in Fall 2016 by
+            five student organizations: Delta Phi Epsilon, UC Berkeley Model United Nations,
+            Berkeley Model United Nations, Sigma Iota Rho, and Amnesty International. The
+            mission of the Council is to facilitate dialogue on global issues and encourage
+            cooperation between organizations related to the field of international relations.
+            In order to raise awareness of global issues and provide students with
+            opportunities to engage with international relations, history and politics on a
+            professional platform, the IRC of Berkeley hosts several semesterly events,
+            including publishing an academic journal and hosting a case competition to foster
+            solutions for pressing international issues.</p>
         </div>
         <OfficerPortrait
           name="OfficerOne"
