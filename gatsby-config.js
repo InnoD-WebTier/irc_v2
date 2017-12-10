60255e2eed2862ad578b924d20dfeedfdad181ce
David Nguyen <d_nguyen@berkeley.edu>
2017-12-09 16:12:28 -0800

    Add transformer plugin for netlify

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,17 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-sass`,
     `gatsby-plugin-eslint`,
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/src/content/case-competitions`,
+        name: 'competitions'
+      }
+    },
+    { 
+      resolve: 'gatsby-transformer-remark', 
+      options: { plugins: [], }
+    },
   ],
   pathPrefix: `/irc_v2`,
 }
