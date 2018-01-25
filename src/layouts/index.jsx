06ea243c6aed6644b622ed14338a9e598c4c3ca8
David Nguyen <me@davidn.win>
2018-01-25 15:13:25 -0800

    This time for real

--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -8,6 +8,7 @@ import '../styles/base.scss';
 const TemplateWrapper = ({ children }) => (
   <div>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+    <script src="https://identity.netlify.com/v1/netlify-identity-widget.js" />
     <Navbar />
     {children()}
     <Footer />
