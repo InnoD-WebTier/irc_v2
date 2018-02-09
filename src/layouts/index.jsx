a08575f5759dc770344639a8c1469d15f24e0141
David Nguyen <me@davidn.win>
2018-02-08 17:42:34 -0800

    Mobile fix

--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -8,6 +8,10 @@ import '../styles/base.scss';
 const TemplateWrapper = ({ children }) => (
   <div>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+    <meta 
+      name='viewport' 
+      content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' 
+    />
     <script src="https://identity.netlify.com/v1/netlify-identity-widget.js" />
     <Navbar />
     {children()}
